461cf23b3c86ca34a846e4de870acd3d06c1ec53
Thomas Daede <daede003@umn.edu>
2018-08-15 13:44:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/hook', function (req, res) {
+  console.log(req.body);
+});
